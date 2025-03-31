89faca9bb2ce32b993fa2b51bbc5a1683a76625c
Akshaj6 <akshaj.samaria@gmail.com>
2025-03-31 17:36:10 +0530

    8

---
+++ Average.py
@@ -0,0 +1,8 @@
+English = int(input("Enter the marks in the first subject :"))
+Hindi = int(input("Enter the marks in the second subject :"))
+Science = int(input("Enter the marks in the third subject :"))
+Maths = int(input("Enter the marks in the fourth subject :"))
+AI = int(input("Enter the marks in the fifth subject :"))
+total = English + Hindi + Science + Maths + AI
+average = total/5
+print(average)
